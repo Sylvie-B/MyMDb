f2a90a7b1a76029703064d931a4e499d7f536c65
Sylvie-B <unsolo@hotmail.fr>
2021-02-11 14:31:38 +0100

    including bootstrap

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My MDb</title>
+    <link rel="stylesheet" href="myCSS.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"
+</head>
+<body>
+
+    <header>
+        <div id="mainTitle">MyMDb</div>
+        <div id="menu">Menu</div>
+        <div id="searchBar">
+            <div id="select">
+                <!-- select -->
+            </div>
+            <div id="research">
+                <!-- input research -->
+            </div>
+        </div>
+        <div id="signIn">Sign in</div>
+    </header>
+
+</body>
+</html>
