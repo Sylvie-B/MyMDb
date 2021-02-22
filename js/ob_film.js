901916ee44db43f87dd92ea9fe25fa98cec0a601
Sylvie-B <unsolo@hotmail.fr>
2021-02-22 13:14:42 +0100

    object

--- js/ob_film.js
+++ js/ob_film.js
@@ -2,9 +2,21 @@ export {FilmObject};
 
 // create a film object
 let FilmObject = {
-    title : "La communauté de l'anneau",
+    type : [
+        "série de films",
+        "médiéval fantastique",
+        ],
+    film : [
+        "La communauté de l'anneau",
+        "Les deux tours",
+        "Le retour du roi"
+    ],
     real : "Peter Jackson",
-    year : "2001",
+    year : [
+        "2001",
+        "2002",
+        "2003"
+        ],
     poster : "./img/poster/lotr01",
     synopsis : "Le plus précieux des anneaux se retrouve en posséssion de l'être le plus inattendu"
 };
