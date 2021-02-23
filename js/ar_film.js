64c6c406dc30d313b515068db1368eb01a5f91d4
Sylvie-B <unsolo@hotmail.fr>
2021-02-23 12:03:50 +0100

    FilmArray

---
+++ js/ar_film.js
@@ -0,0 +1,11 @@
+export {FilmArray};
+
+// create a film object
+let FilmArray = [
+    './img/poster/hobbit01.jpg',
+    './img/poster/hobbit02.jpg',
+    './img/poster/hobbit03.jpg',
+    './img/poster/lotr01.jpg',
+    './img/poster/lotr02.jpg',
+    './img/poster/lotr03.jpg',
+]
--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,17 @@
+import {FilmArray} from './ar_film';
+
 let container = document.getElementById('container');
 container.style.height = window.innerHeight + 'px';
 container.style.width = window.innerWidth + 'px';
 
 $('.carousel').carousel();
+
+// create img for all film + add on allFilms section
+
+
+// create .carousel-item div * film + add .active to the first div
+// append child all film img
+
+
+// thumbnails : get genre + same genre film
+
